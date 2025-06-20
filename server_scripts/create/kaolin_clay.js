81c9431be06569dc614be1a46d3cfc9916f68423
10MeV <863649106@qq.com>
2025-06-20 18:20:51 +0800

    25.6.20

--- server_scripts/create/kaolin_clay.js
+++ server_scripts/create/kaolin_clay.js
@@ -8,7 +8,7 @@ ServerEvents.recipes(e => {
         e.recipes.create.pressing("tfcr:roasted_kaolin_clay","tfcr:roasted_kaolin_clay"),
         e.recipes.createDeploying("tfcr:roasted_kaolin_clay", ["tfcr:roasted_kaolin_clay", 'tfc:metal/hammer/wrought_iron']).keepHeldItem()
        ]).transitionalItem("tfcr:roasted_kaolin_clay").loops(3)
-        e.recipes.create.crushing("tfcr:roasted/bauxite","tfcr:roasted_brick/bauxite")
+        e.recipes.create.crushing("4x tfcr:roasted/bauxite","tfcr:roasted_brick/bauxite")
         e.recipes.create.milling(["tfc:kaolin_clay",Item.of("tfc:kaolin_clay").withChance(0.2)],"tfcr:raw_kaolin_clay")
         e.recipes.create.mixing("tfc:powder/kaolinite","3x tfc:kaolin_clay").superheated()
 })
