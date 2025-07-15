d0f3f03ad8dce4e10dacea70b966c0006d66a2cd
10MeV <863649106@qq.com>
2025-07-15 21:10:04 +0800

    25.7.15

--- startup_scripts/addBlocks.js
+++ startup_scripts/addBlocks.js
@@ -1,5 +1,5 @@
 StartupEvents.registry("block",event=>{
     event.create("kubejs:bloomery_io","basic")
-    event.create("tfcr:laser_engraver_idle","basic")
+    event.create("tfcr:laser_engraver_idle","basic").hardness(0.5)
     event.create("tfcr:laser_engraver_work","basic")
 })
--- startup_scripts/registry.js
+++ startup_scripts/registry.js
@@ -63,4 +63,39 @@ StartupEvents.registry('item', event => {
 	.texture('kubejs:item/seed')
 	.displayName('种子奖励袋')
 
+	//电子元件
+	event.create('tfcr:ec_r').texture('tfcr:item/1').displayName('电阻')
+	event.create('tfcr:ec_c').texture('tfcr:item/2').displayName('电容')
+	event.create('tfcr:ec_d').texture('tfcr:item/3').displayName('二极管')
+	event.create('tfcr:ec_q').texture('tfcr:item/4').displayName('晶体管')
+	event.create('tfcr:ec_l').texture('tfcr:item/23').displayName('电感')
+
+	//光刻相关
+	event.create('tfcr:scs').texture('tfcr:item/5').displayName('单晶硅')
+	event.create('tfcr:wafer').texture('tfcr:item/6').displayName('晶圆')
+	event.create('tfcr:w_ic').texture('tfcr:item/7').displayName('IC晶圆')
+	event.create('tfcr:ccd_ic').texture('tfcr:item/8').displayName('IC硅片')
+	event.create('tfcr:w_ram').texture('tfcr:item/9').displayName('RAM晶圆')
+	event.create('tfcr:ccd_ram').texture('tfcr:item/10').displayName('RAM硅片')
+	event.create('tfcr:w_soc').texture('tfcr:item/11').displayName('SOC晶圆')
+	event.create('tfcr:ccd_soc').texture('tfcr:item/12').displayName('SOC硅片')
+	event.create('tfcr:w_qsoc').texture('tfcr:item/19').displayName('量子SOC晶圆')
+	event.create('tfcr:ccd_qsoc').texture('tfcr:item/20').displayName('量子SOC硅片')
+
+	//电路板相关
+	event.create('tfcr:ic_1').texture('tfcr:item/13').displayName('计算机')
+	event.create('tfcr:ic_2').texture('tfcr:item/14').displayName('计算机集群')
+	event.create('tfcr:ic_3').texture('tfcr:item/15').displayName('超算单元')
+	event.create('tfcr:ic_4').texture('tfcr:item/16').displayName('量子计算机')
+	event.create('tfcr:ic_5').texture('tfcr:item/17').displayName('量子计算机集群')
+	event.create('tfcr:ic_6').texture('tfcr:item/18').displayName('量子超算单元')
+
+	//电路板基板
+	event.create('tfcr:cbs_basic').texture('tfcr:item/21').displayName('基础pcb基板')
+	event.create('tfcr:cbs_ultra').texture('tfcr:item/22').displayName('高级pcb基板')
+
+
+	
+
+
 })
